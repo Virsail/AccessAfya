947e361a504fa5e6fc49f741f9a8ede21e587394
virsail <virsaileric@gmail.com>
2021-06-26 10:58:49 +0300

    import appheaderdropdown from appheaderdropdown

--- src/components/header/AppHeaderDropdown.js
+++ src/components/header/AppHeaderDropdown.js
@@ -15,36 +15,36 @@ import {
     return (
       <CDropdown variant="nav-item">
         <CDropdownToggle placement="bottom-end" className="py-0" caret={false}>
-          <CAvatar src="avatars/8.jpg" size="md" />
+          <CAvatar src="avatars/avatar8.jpg" size="md" />
         </CDropdownToggle>
         <CDropdownMenu className="pt-0" placement="bottom-end">
-          <CDropdownHeader className="bg-light fw-semibold py-2">Account</CDropdownHeader>
+          <CDropdownHeader className="bg-dark fw-semibold py-2">Account</CDropdownHeader>
           <CDropdownItem href="#">
             <CIcon name="cil-bell" className="me-2" />
             Updates
             <CBadge color="info" className="ms-2">
-              42
+              12
             </CBadge>
           </CDropdownItem>
           <CDropdownItem href="#">
             <CIcon name="cil-envelope-open" className="me-2" />
             Messages
             <CBadge color="success" className="ms-2">
-              42
+              45
             </CBadge>
           </CDropdownItem>
           <CDropdownItem href="#">
             <CIcon name="cil-task" className="me-2" />
             Tasks
             <CBadge color="danger" className="ms-2">
-              42
+              2
             </CBadge>
           </CDropdownItem>
           <CDropdownItem href="#">
             <CIcon name="cil-comment-square" className="me-2" />
             Comments
             <CBadge color="warning" className="ms-2">
-              42
+              4
             </CBadge>
           </CDropdownItem>
           <CDropdownHeader className="bg-light fw-semibold py-2">Settings</CDropdownHeader>
@@ -56,25 +56,10 @@ import {
             <CIcon name="cil-settings" className="me-2" />
             Settings
           </CDropdownItem>
-          <CDropdownItem href="#">
-            <CIcon name="cil-credit-card" className="me-2" />
-            Payments
-            <CBadge color="secondary" className="ms-2">
-              42
-            </CBadge>
-          </CDropdownItem>
-          <CDropdownItem href="#">
-            <CIcon name="cil-file" className="me-2" />
-            Projects
-            <CBadge color="primary" className="ms-2">
-              42
-            </CBadge>
-          </CDropdownItem>
+          
+          
           <CDropdownDivider />
-          <CDropdownItem href="#">
-            <CIcon name="cil-lock-locked" className="me-2" />
-            Lock Account
-          </CDropdownItem>
+        
         </CDropdownMenu>
       </CDropdown>
     )
---
+++ src/components/header/index.js
@@ -0,0 +1,1 @@
+import AppHeaderDropdown from './AppHeaderDropdown'
