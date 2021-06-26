7db27d2615f51e0386994f29e9e0053270da5193
virsail <virsaileric@gmail.com>
2021-06-26 17:45:18 +0300

    import dahsboard component into apptest.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,8 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { shallow } from 'enzyme/build'
 import App from './App';
+import Dashboard from './views/dashboard/Dashboard.js'
 
 test('renders learn react link', () => {
   render(<App />);
