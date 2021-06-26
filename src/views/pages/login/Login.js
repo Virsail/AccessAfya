157773eae5fc3ac840d8699978511244bc61da77
virsail <virsaileric@gmail.com>
2021-06-26 22:45:04 +0300

    import CIcon from coreui/icons-react

--- src/views/pages/login/Login.js
+++ src/views/pages/login/Login.js
@@ -1,2 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import {
+    CButton,
+    CCard,
+    CCardBody,
+    CCardGroup,
+    CCol,
+    CContainer,
+    CForm,
+    CFormControl,
+    CInputGroup,
+    CInputGroupText,
+    CRow,
+  } from '@coreui/react'
+  import CIcon from '@coreui/icons-react'
