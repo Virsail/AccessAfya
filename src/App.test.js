057ae48c7ff23a36f7c66eba33235971072b48e0
virsail <virsaileric@gmail.com>
2021-06-26 17:54:06 +0300

    import the CAvatar,CButton from @coreui/react

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,19 @@ import { shallow } from 'enzyme/build'
 import App from './App';
 import Dashboard from './views/dashboard/Dashboard.js'
 
+
+it('mounts App without crashing', () => {
+  const wrapper = shallow(<App />)
+  wrapper.unmount()
+})
+
+
+
+it('mounts Dashboard without crashing', () => {
+  const wrapper = shallow(<Dashboard />)
+  wrapper.unmount()
+})
+
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
---
+++ src/views/dashboard/Dashboard.js
@@ -0,0 +1,21 @@
+import React ,{lazy} from 'react'
+
+
+import {
+    CAvatar,
+    CButton,
+    CButtonGroup,
+    CCard,
+    CCardBody,
+    CCardFooter,
+    CCardHeader,
+    CCol,
+    CProgress,
+    CRow,
+    CTable,
+    CTableBody,
+    CTableDataCell,
+    CTableHead,
+    CTableHeaderCell,
+    CTableRow,
+  } from '@coreui/react'
