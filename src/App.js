e5d0431f7b1920e7fcce622c5dd142d046957e53
virsail <virsaileric@gmail.com>
2021-06-27 04:30:37 +0300

    delete the app.css file

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import './scss/style.scss'
 
 
+
 const loading = (
   <div className="pt-3 text-center">
     <div className="sk-spinner sk-spinner-pulse"></div>
