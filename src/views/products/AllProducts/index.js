fd6edd94415a8d638cc826d6fcdfb80c189f050a
virsail <virsaileric@gmail.com>
2021-06-27 16:56:42 +0300

    create a colors component that will contain diffrent themes for the application

---
+++ src/views/products/AllProducts/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+/**
+ * @author
+ * @function AllProducts
+ **/
+
+const AllProducts = (props) => {
+  return <div>Hello I am All Products</div>
+}
+export default AllProducts
