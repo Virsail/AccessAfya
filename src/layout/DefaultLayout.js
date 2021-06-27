4395fdf31e3a731c1f74fdd4fa143cd862183dea
virsail <virsaileric@gmail.com>
2021-06-27 16:36:09 +0300

    fix the import error in my components folder

--- src/layout/DefaultLayout.js
+++ src/layout/DefaultLayout.js
@@ -1,34 +1,19 @@
 import React from 'react'
-import { CCard, CCardBody, CCardHeader, CCol, CRow } from '@coreui/react'
-import CIcon from '@coreui/icons-react'
-import { brandSet } from '@coreui/icons'
-import { DocsLink } from 'src/reusable'
+import { AppContent, AppSidebar, AppFooter, AppHeader } from '../components/index'
 
-const toKebabCase = (str) => {
-  return str.replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '$1-$2').toLowerCase()
-}
-
-export const getIconsView = (iconset) => {
-  return Object.entries(iconset).map(([name, value]) => (
-    <CCol className="mb-5" xs="6" sm="4" md="3" xl="2" key={name}>
-      <CIcon content={value} size="2xl" />
-      <div>{toKebabCase(name)}</div>
-    </CCol>
-  ))
-}
-
-const CoreUIIcons = () => {
+const DefaultLayout = () => {
   return (
-    <CCard className="mb-4">
-      <CCardHeader>
-        Brand Icons
-        <DocsLink href="https://github.com/coreui/coreui-icons" text="GitHub" />
-      </CCardHeader>
-      <CCardBody>
-        <CRow className="text-center">{getIconsView(brandSet)}</CRow>
-      </CCardBody>
-    </CCard>
+    <div>
+      <AppSidebar />
+      <div className="wrapper d-flex flex-column min-vh-100 bg-light">
+        <AppHeader />
+        <div className="body flex-grow-1 px-3">
+          <AppContent />
+        </div>
+        <AppFooter />
+      </div>
+    </div>
   )
 }
 
-export default CoreUIIcons
+export default DefaultLayout
